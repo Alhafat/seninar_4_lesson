0eecdad03e0ce67b2dc9a71c379bd7d1c32a8b48
Alhafat <vladimir-lukin@inbox.ru>
2023-01-24 21:29:29 +0300

    exp_3

---
+++ exp_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+
+
+int GetProduct (int number)
+{
+    int Product = 1;
+    if (number > 1) 
+    {
+        for (int i = 1; i <= number; i++) // i = i + 1 // i += 1
+        {
+            Product *= i; // sum = sum * i
+        }
+    }
+
+    return Product ;
+}
+
+Console.Write("Введите number: ");
+int number = int.Parse(Console.ReadLine()!);
+int Product  = GetProduct(number);
+Console.WriteLine($"{Product}");
