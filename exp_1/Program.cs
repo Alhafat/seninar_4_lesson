6430a573bc0a8bdd181cdd961724eceee70d44af
Alhafat <vladimir-lukin@inbox.ru>
2023-01-24 23:22:04 +0300

    exp_3

--- exp_1/Program.cs
+++ exp_1/Program.cs
@@ -1,42 +1,23 @@
 ﻿// Напишите программу, которая принимает на вход число A
 // и выдаёт сумму чисел от 1 до A
 
-// int GetSum(int A)
-// {
-//     int sum = 0;
-//     if (A > 1) 
-//     {
-//         for (int i = 1; i <= A; i++) // i = i + 1 // i += 1
-//         {
-//             sum += i; // sum = sum + i
-//         }
-//     }
-
-//     return sum;
-// }
-
-// Console.Write("Введите А: ");
-// int n = int.Parse(Console.ReadLine()!);
-// int sum = GetSum(n);
-// Console.WriteLine($"{sum}");
-
 Console.Clear();
 
 
-int GetSum(int A)
+int GetSum(int A)                       // int GetSum(int A)
 {
-    int sum=0;
-    if (A>1)
+      int sum=0;                          //     int sum = 0;
+    if (A>=1)                            //     if (A > 1) 
     {
-        for (int i=0; i<=A; i++)
+        for (int i=0; i<=A; i++)        // for (int i = 1; i <= A; i++)
         {
-        sum+=i;
+        sum+=i;                         // sum += i;
         }
     }
-    return sum;
+    return sum;                         //return sum;
 }
 
-Console.Write("Введите число: ");
-int number=int.Parse(Console.ReadLine()!);
-int sum=GetSum(number);
-Console.WriteLine($"{sum}");
+Console.Write("Введите число: ");               // Console.Write("Введите А: ");
+int number=int.Parse(Console.ReadLine()!);      // int n = int.Parse(Console.ReadLine()!);
+int sum=GetSum(number);                         // int sum = GetSum(n);
+Console.WriteLine($"{sum}");                    // Console.WriteLine($"{sum}");
---
+++ exp_4/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+void FillArray(int[] nums) 
+{
+    int len=nums.Length;
+    int index=0;
+    while (index<len)
+    {
+        nums[index]=new Random().Next(0,2); //добавляет случайные элементы в массив
+        index++;
+    }
+}
+
+
+void PrintArray(int[] col)
+{
+    int count=col.Length;
+    int position=0;
+    while (position<count)
+    {
+        col[position]=new Random().Next(0,2);
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+
+Console.Write("Введите number: ");
+int number = int.Parse(Console.ReadLine()!);
+int[] numbers=new int[number]; //определяет массив из number элементов
+
+FillArray(numbers); //заполняет массив
+PrintArray(numbers); //выводит(печатает массив)
+Console.WriteLine(String.Join(", ", numbers));
