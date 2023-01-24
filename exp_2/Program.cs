38afe9e4b4cd605d4deeee5d179bfdeb7e3df048
Alhafat <vladimir-lukin@inbox.ru>
2023-01-24 21:18:53 +0300

    exp_2

---
+++ exp_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Введите число: ");
+int number=int.Parse(Console.ReadLine()!);
+
+int i=0;
+
+while (number>0)
+{
+    number=number/10;
+    i++;
+}
+Console.WriteLine(i);
